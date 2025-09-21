82fa4ee2820d79b2d11aac6d7482d218d950a229
Grigory163 <kotenkovgrigorij@gmail.com>
2025-09-21 07:59:15 -0400

    commit1

--- Parallel_prog-lab_1/generate_write.cpp
+++ Parallel_prog-lab_1/generate_write.cpp
@@ -9,92 +9,99 @@
 
 using namespace std;
 
-auto engine = std::mt19937(std::time(nullptr));
+auto random_generator = std::mt19937(std::time(nullptr));
 
-vector<vector<int>> generate(size_t size) {
-	vector<vector<int>> matrix(size, vector<int>(size)); 
-	for (auto& i : matrix) {
-		for (int& j : i) {
-			j = engine() % RAND_MAX;
-		}
-	}
-	return matrix;
+vector<vector<int>> create_matrix(size_t dimension) {
+    vector<vector<int>> matrix(dimension, vector<int>(dimension)); 
+    for (auto& row : matrix) {
+        for (int& element : row) {
+            element = random_generator() % RAND_MAX;
+        }
+    }
+    return matrix;
 }
 
-
-void write_to_file(const vector<vector<int>>& matrix, const string& path) {
-	std::ofstream out(path);
-	for ( const auto& i : matrix) {
-		for (int j : i) 
-			out << j << " ";
-		out << std::endl;
-	}
+void save_matrix(const vector<vector<int>>& matrix, const string& filename) {
+    ofstream output_file(filename);
+    for (const auto& row : matrix) {
+        for (int value : row) {
+            output_file << value << " ";
+        }
+        output_file << endl;
+    }
 }
 
+vector<vector<int>> load_matrix(const string& filename) {
+    ifstream input_file(filename);
+    vector<vector<int>> matrix;
+    string line;
 
-vector<vector<int>> read_from_file(const string& path) {
-	std::ifstream in(path);
-	vector<vector<int>> matrix;
-	string line;
-
-	while (getline(in, line)) {
-		istringstream iss(line);
-		vector<int> row;
-		int value;
-		while (iss >> value) {
-			row.push_back(value);
-		}
-		if (!row.empty()) {
-			matrix.push_back(row);
-		}
-	}
-	return matrix;
+    while (getline(input_file, line)) {
+        istringstream stream(line);
+        vector<int> row;
+        int number;
+        while (stream >> number) {
+            row.push_back(number);
+        }
+        if (!row.empty()) {
+            matrix.push_back(row);
+        }
+    }
+    return matrix;
 }
 
-
-vector<vector<int>> mul_matrix(const vector<vector<int>>& matrix1, const vector<vector<int>>& matrix2) {
-	size_t size = matrix1.size();
-	vector<vector<int>> result(size, vector<int>(size, 0));
-	for (int i = 0; i < size; ++i) {
-		for (int j = 0; j < size; ++j) {
-			for (int k = 0; k < size; ++k) {
-				result[i][j] += matrix1[i][k] * matrix2[k][j];
-			}
-		}
-	}
-	return result;
+vector<vector<int>> multiply_matrices(const vector<vector<int>>& first_matrix, const vector<vector<int>>& second_matrix) {
+    size_t dimension = first_matrix.size();
+    vector<vector<int>> result_matrix(dimension, vector<int>(dimension, 0));
+    
+    for (int i = 0; i < dimension; ++i) {
+        for (int j = 0; j < dimension; ++j) {
+            for (int k = 0; k < dimension; ++k) {
+                result_matrix[i][j] += first_matrix[i][k] * second_matrix[k][j];
+            }
+        }
+    }
+    return result_matrix;
 }
 
 int main() {
+    vector<int> dimensions = {50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1500, 2000, 3000};
+    
+    // Создание тестовых матриц
+    for (const auto& dim : dimensions) {
+        for (int num = 1; num < 3; ++num) {
+            vector<vector<int>> test_matrix = create_matrix(dim);
+            string filename = to_string(num) + "_matrix_" + to_string(dim) + ".txt";
+            save_matrix(test_matrix, filename);
+        }
+    }
 
-	vector<int> counts = { 50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1500, 2000, 3000};
-	for (const auto& count : counts) {
-		for (int i = 1; i < 3; ++i) {
-			vector<vector<int>> matrix = generate(count);
-			string path = std::to_string(i)+ "_" + std::to_string(count) + ".txt";
-			write_to_file(matrix, path);
-		}
-	}
-	vector<double> times(counts.size(), 0.0);
-		for (size_t i = 0; i < counts.size(); ++i) {
-			int count = counts[i];
-			string path_1 = "1_" + std::to_string(count) + ".txt";
-			string path_2 = "2_" + std::to_string(count) + ".txt";
-			string result_path = "result_" + std::to_string(count) + ".txt";
+    vector<double> execution_times(dimensions.size(), 0.0);
+    
+    // Измерение времени умножения
+    for (size_t idx = 0; idx < dimensions.size(); ++idx) {
+        int current_dim = dimensions[idx];
+        string file1 = "1_" + to_string(current_dim) + ".txt";
+        string file2 = "2" + to_string(current_dim) + ".txt";
+        string result_file = "result_matrix_" + to_string(current_dim) + ".txt";
 
-			auto start_time = chrono::steady_clock::now();
-			vector<vector<int>> matrix_1 = read_from_file(path_1);
-			vector<vector<int>> matrix_2 = read_from_file(path_2);
-			vector<vector<int>> result = mul_matrix(matrix_1, matrix_2);
-			write_to_file(result, result_path);
-			auto end_time = chrono::steady_clock::now();
+        auto start = chrono::steady_clock::now();
+        
+        vector<vector<int>> matrix_a = load_matrix(file1);
+        vector<vector<int>> matrix_b = load_matrix(file2);
+        vector<vector<int>> multiplication_result = multiply_matrices(matrix_a, matrix_b);
+        
+        save_matrix(multiplication_result, result_file);
+        
+        auto end = chrono::steady_clock::now();
+        execution_times[idx] = chrono::duration<double, milli>(end - start).count();
+    }
 
-			times[i] = chrono::duration<double, std::milli>(end_time - start_time).count();
-		}
-	std::ofstream out("stats.txt");
-	for (size_t i = 0; i < counts.size(); ++i) {
-		out << counts[i] << ": " << (times[i]) << "\n";
-		out << std::endl;
-	}
+    // Сохранение результатов измерений
+    ofstream results_output("stats.txt");
+    for (size_t idx = 0; idx < dimensions.size(); ++idx) {
+        results_output << dimensions[idx] << ": " << execution_times[idx] << " ms" << endl;
+    }
 
+    return 0;
 }
